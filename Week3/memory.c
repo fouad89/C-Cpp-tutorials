78b2955b05febdb2250a160db798bf3cb108634e
Fouad <fouad89ahmed@gmail.com>
2021-07-08 13:13:43 +0100

    ignore exe files

---
+++ Week3/memory.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int age = 30;
+    double gpa = 3.4;
+    char grade = 'A';
+    // pointer for age variable
+    // use the same data type as the variable. 
+    // conventional names starts with p and Var name
+    int *pAge = &age;
+    double *pGpa = &gpa;
+    char *pGrade = &grade;
+
+    // to get physical memory address, %p for pointer
+    printf("age: %p\ngpa: %p\ngrade: %p\n", &age, &gpa, &grade);
+
+    // pointers are a type of data that can be used in the program
+    printf("%p\n", pAge);
+    // derefrencing is getting the value from the memory address
+    printf("%d\n", *pAge);
+
+    return 0;
+}
